c087b71149abf3154d43e39da83f4a8fa4d994e6
Ryan Fredericks <Kurt@stephanies-mbp.myfiosgateway.com>
2024-09-09 18:00:43 -0400

    Created array of questions

--- Develop/index.js
+++ Develop/index.js
@@ -4,7 +4,34 @@ import fs from 'fs';
 // TODO: Create an array of questions for user input
 const questions = [
 {
-    message: ''     
+    type: 'input',
+    name: 'title',
+    message: 'Enter project title: '
+},
+{
+    type: 'input',
+    name: 'description',
+    message: 'Enter your project description: '
+},
+{
+    type: 'input',
+    name: 'installation instructions',
+    message: 'Enter installation instructions: '
+},
+{
+    type: 'input',
+    name: 'usage information',
+    message: 'Enter usage information:'
+},
+{
+    type: 'input',
+    name: 'contributing',
+    message: 'Enter contribution guidelines:'
+},
+{
+type: 'input',
+    name: 'test instructions',
+    message: 'Enter test instructions:'
 }
 ];
 
